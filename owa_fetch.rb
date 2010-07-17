d4f7f5f42f4e4f462cdc6c98047d41ac172f2c2c
Duane Johnson <duane.johnson@gmail.com>
2010-07-16 22:05:48 -0500

    Cleanup root

--- owa_fetch.rb
+++
@@ -1,6 +0,0 @@
-require 'rubygems'
-require 'nokogiri'
-
-$fetch_day_url = %{https://itxmail01/owa/?ae=Folder&t=IPF.Appointment&yr=%s&mn=%s&dy=%s}
-$login_url = %{https://itxmail01/owa/auth/logon.aspx}
-
